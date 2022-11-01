ffa159528b0b861c191751fc10d5a8afb42862ca
Serhii0406 <106251203+Serhii0406@users.noreply.github.com>
2022-11-01 20:19:11 +0200

    task 1
    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    styleWindow: document.querySelector('body')
+}
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        if (timerId) {
+         refs.startBtn.disabled = true; 
+      }
+    refs.styleWindow.style.backgroundColor = getRandomHexColor();  
+  }, 1000);
+});
+
+refs.stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+});
